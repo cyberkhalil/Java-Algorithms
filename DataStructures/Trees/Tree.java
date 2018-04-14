2336697c966a3f17125c61c70694fc7a72aa5104
khalil2535 <khalil2535@localhost>
2018-04-14 06:12:38 +0300

    Update Tree.java

--- DataStructures/Trees/Tree.java
+++ DataStructures/Trees/Tree.java
@@ -10,17 +10,17 @@ import java.util.ArrayList;
  */
 public class Tree<E> {
 
-    private Node root;
+    private Node<E> root;
 
     /**
      *
      * @param item
      */
     public Tree(E item) {
-        this.root = new Node(item);
+        this.root = new Node<>(item);
     }
 
-    public Tree(Node root) {
+    public Tree(Node<E> root) {
         this.root = root;
     }
 
@@ -30,7 +30,7 @@ public class Tree<E> {
         } else {
             String myTree = root.toString();
             for (int i = 0; i < root.getChildernNumber(); i++) {
-                myTree += "\n  " + new Tree((Node) root.getChildrens().get(i)).preOrderWithS("      ");
+                myTree += "\n  " + new Tree<>(root.getChildrens().get(i)).preOrderWithS("      ");
             }
             return myTree;
         }
@@ -42,7 +42,7 @@ public class Tree<E> {
         } else {
             String myTree = root.toString();
             for (int i = 0; i < root.getChildernNumber(); i++) {
-                myTree += "\n " + s + new Tree((Node) root.getChildrens().get(i)).preOrderWithS(s + s);
+                myTree += "\n " + s + new Tree<>(root.getChildrens().get(i)).preOrderWithS(s + s);
             }
             return myTree;
         }
@@ -55,11 +55,11 @@ public class Tree<E> {
 
             String myTree = "";
             for (int i = 0; i < root.getChildernNumber() / 2; i++) {
-                myTree += "(" + new Tree((Node) root.getChildrens().get(i)).inOrder();
+                myTree += "(" + new Tree<>(root.getChildrens().get(i)).inOrder();
             }
             myTree += root.toString();
             for (int i = root.getChildernNumber() / 2; i < root.getChildernNumber(); i++) {
-                myTree += new Tree((Node) root.getChildrens().get(i)).inOrder() + ")";
+                myTree += new Tree<>(root.getChildrens().get(i)).inOrder() + ")";
             }
             return myTree;
         }
@@ -70,36 +70,36 @@ public class Tree<E> {
     }
 
     public boolean addToNode(int i, E item) {
-        return ((Node) getRoot().getChildrens().get(i)).addChildren(item);
+        return getRoot().getChildrens().get(i).addChildren(item);
     }
 
     /**
      * @return the root
      */
-    public Node getRoot() {
+    public Node<E> getRoot() {
         return root;
     }
 
     /**
      * @param root the root to set
      */
-    public void setRoot(Node root) {
+    public void setRoot(Node<E> root) {
         this.root = root;
     }
 }
 
 class Node<E> {
 
-    private ArrayList<Node> children;
+    private final ArrayList<Node<E>> children;
     private final E item;
 
     Node(E item) {
-        children = new ArrayList<>();
+        children = new ArrayList<>(2);
         this.item = item;
     }
 
     public boolean addChildren(E childItem) {
-        return this.children.add(new Node(childItem));
+        return this.children.add(new Node<>(childItem));
     }
 
     @Override
@@ -107,7 +107,7 @@ class Node<E> {
         return this.item.toString();
     }
 
-    public ArrayList<Node> getChildrens() {
+    public ArrayList<Node<E>> getChildrens() {
         return this.children;
     }
 
